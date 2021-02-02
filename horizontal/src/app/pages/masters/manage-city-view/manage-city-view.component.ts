33207105cc9066643bfdfabe4b5a0007b956b5c0
nadeore <nadeore@gmail.com>
2021-02-02 17:05:49 +0530

    updated

--- horizontal/src/app/pages/masters/manage-city-view/manage-city-view.component.ts
+++ horizontal/src/app/pages/masters/manage-city-view/manage-city-view.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'manage-city-view',
   templateUrl: './manage-city-view.component.html',
   styleUrls: ['./manage-city-view.component.scss']
 })
 export class ManageCityViewComponent implements OnInit {
+  cityForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.cityForm = this.fb.group({
+      cityName: ['', Validators.required]
+    });
   }
 
 }
--- horizontal/src/app/pages/masters/manage-city-view/manage-city.module.ts
+++ horizontal/src/app/pages/masters/manage-city-view/manage-city.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import {SharedModule} from '../../../shared/shared.module';
 import {ManageCityViewComponent} from './manage-city-view.component';
 import {ManageCityRoutingModule} from './manage-city-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [ManageCityViewComponent],
-  imports: [
-    CommonModule,
-    ManageCityRoutingModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        ManageCityRoutingModule,
+        SharedModule,
+        ReactiveFormsModule
+    ]
 })
 export class ManageCityModule { }
